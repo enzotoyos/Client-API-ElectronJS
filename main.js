b16a9837027c2bd2007620208a976276555cd4b5
enzotoyos <enzo.toyos@gmail.com>
2022-02-28 21:13:06 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+      // gestion du darkMode
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
+
+  }
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
+  
+
+
+  app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      })
+  })
